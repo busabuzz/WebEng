c13e334c3d04392f22a84c582010b0b9cb697078
busabuzz <bas.de.walle@gmail.com>
2019-03-11 11:09:49 +0100

    added setup

---
+++ README.txt
@@ -0,0 +1,1 @@
+run python setup.py to install dependencies
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='airlines_api',
+    version='0.1dev',
+    packages=['airlines_api'],
+    install_requires=[
+        'connexion',
+    ],
+)
